52b8cd782ed1f9b8014e8b4862ae56046482ceb6
Eric Della <ericdella13@gmail.com>
2021-01-17 20:25:56 -0800

    functionality to Login.js

--- friends/src/components/Login.js
+++ friends/src/components/Login.js
@@ -1,10 +1,54 @@
+import axios from "axios";
 import React from "react";
 
 class Login extends React.Component {
+    state = {
+        userInfo:{
+            username: "",
+            password: "",
+        }
+    }
+
+    handleChange = e => {
+        this.setState({
+            userInfo: {
+                ...this.state,
+                [e.target.name]: e.target.value
+            }
+        })
+    }
+
+    login = e => {
+        e.preventDefault();
+        axios.post("http://localhost:5000/api/login", this.state.userInfo)
+        .then(res => {
+            localStorage.setItem("token", res.data.payload);
+            this.props.history.push("/protected")
+        })
+        .catch(err => {
+            console.log(err)
+        })
+    }
 
     render() {
         return (
-            <h1>HI</h1>
+            <div>
+            <form onSubmit={this.login}>
+              <input
+                type="text"
+                name="username"
+                value={this.state.userInfo.username}
+                onChange={this.handleChange}
+              />
+              <input
+                type="password"
+                name="password"
+                value={this.state.userInfo.password}
+                onChange={this.handleChange}
+              />
+              <button>Log in</button>
+            </form>
+          </div>
         )
     }
 }
