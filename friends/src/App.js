40fa552e78af38291045a4c7f512a114063506f5
Eric Della <ericdella13@gmail.com>
2021-01-17 20:52:40 -0800

    added FriendsList.js file, also link in App.js

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
 import './App.css';
 import Login from "./components/Login"
+import FriendsList from "./components/FriendsList"
 
 function App() {
   return (
@@ -8,11 +9,15 @@ function App() {
     <div className="App">
       <ul>
         <li>
-        <Link to="/login">Login</Link>
+          <Link to="/login">Login</Link>
+        </li>
+        <li>
+          <Link to="/friendslist">Friends List</Link>
         </li>
       </ul>
       <Switch>
         <Route path="/login" component={Login}/>
+        <Route path="/friendslist" component={FriendsList} />
       </Switch>
 
     </div>
---
+++ friends/src/components/FriendsList.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+
+class FriendsList extends React.Component {
+
+    render() {
+        return (
+            <h1>FRIENDS LIST</h1>
+        )
+    }
+}
+
+export default FriendsList
